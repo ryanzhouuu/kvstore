343d67088dffbe97c7577adb7d133be472a98439
Ryan Zhou <ryanzhouuu@gmail.com>
2025-12-02 00:28:31 -0600

    Define variables and methods for KVStore class.

--- include/KVStore.hpp
+++ include/KVStore.hpp
@@ -1,5 +1,14 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+#include <shared_mutex>
 
 class KVStore {
-
+private:
+    std::unordered_map<std::string, std::string> data_;
+    mutable std::shared_timed_mutex mutex; 
+public:
+    void set(const std::string& key, const std::string& value);
+    std::string get(const std::string& key);
+    bool remove(const std::string& key);
 };
